1b81eda36bc23dd819df096db33c67987ba1da22
topspinz <matthew.levstek@gmail.com>
2023-11-28 15:35:15 -0500

    built js back end to tie the calculator together

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-var firstNumber=0;
-var secondNumber=0;
+var stackNumber=0;
+var operand = [];
+firstNumber = null;
+secondNumber = null;
+currentOperand = null;
+var numEqualClicks = 0;
+
+const operate = (firstNumber,operator,secondNumber) => {
+    const calcFunctions = {
+        '+':add(firstNumber,secondNumber),
+        '-':subtract(firstNumber,secondNumber),
+        'x':multiply(firstNumber,secondNumber),
+        '/':divide(firstNumber,secondNumber),
+    };
+
+    return String(calcFunctions[operator]);
+}
 
 function add(a,b){
     return a+b
@@ -14,13 +29,46 @@ function divide(a,b){
     return a/b
 }
 
-const operate = (operator,firstNumber,secondNumber) => {
-    const calcFunctions = {
-        '+':add(firstNumber,secondNumber),
-        '-':subtract(firstNumber,secondNumber),
-        '*':multiply(firstNumber,secondNumber),
-        '/':divide(firstNumber,secondNumber),
-    };
+const clearCalc = () => {
+    return operand=[],stack=[],stackNumber=0,display.innerHTML=0;
+};
 
-    return calcFunctions[operator];
-}
+
+const btnOperands = document.getElementById("container");
+const display = document.getElementById("display");
+
+
+btnOperands.addEventListener("click", function (e) {
+   
+
+    if(e.target.className=="operand"){
+        
+        operand.push(e.target.value);
+        const displayValue = operand.reduce((x,y)=> x + y,);
+        display.innerHTML = displayValue;
+        
+    } else if (e.target.className=="operator"){
+        if (firstNumber==null) {
+            currentOperand = e.target.innerHTML;
+            firstNumber = display.innerHTML;
+        } else if (firstNumber!=null){
+            secondNumber=display.innerHTML;
+        }
+        if (firstNumber!=null && secondNumber!=null && currentOperand!=null){
+           display.innerHTML =operate(Number(firstNumber),currentOperand,Number(secondNumber));
+           firstNumber=display.innerHTML;
+           secondNumber=null;
+           currentOperand= e.target.innerHTML;
+        }
+        operand = [];
+    } else if (e.target.className=="clear"){
+        clearCalc();
+    } else if (e.target.className=="equals"){
+        if (firstNumber==null && secondNumber==null && currentOperand==null) return  0;
+            secondNumber=display.innerHTML;
+            display.innerHTML= operate(Number(firstNumber),currentOperand,Number(secondNumber));
+            operand = [];
+            firstNumber=null;
+            secondNumber=null;
+    }
+  });
